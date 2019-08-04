155b4efbb976b2274266a3808261102d6fdedbf9
Joviane <joviane@gmail.com>
2019-08-04 23:19:45 +0100

    Creating project with Spring STS

---
+++ src/main/java/training/collabcode/qattus/QattusApiApplication.java
@@ -0,0 +1,13 @@
+package training.collabcode.qattus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QattusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QattusApiApplication.class, args);
+	}
+
+}
